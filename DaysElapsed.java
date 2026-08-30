fe7bfdc2fade0790e652f2b790e80bf3c0380bbd
donikenaganesh-1822 <donikenaganesh@klh.edu.in>
2026-08-31 00:15:27 +0530

    Add files via upload

---
+++ DaysElapsed.java
@@ -0,0 +1,27 @@
+public class DaysElapsed {
+    public static void main(String[] args) {
+        int month = 12;
+        int day = 31;
+        int total = day;
+        boolean valid = true;
+
+        switch (month) {
+            case 12: total += 30; // November
+            case 11: total += 31; // October
+            case 10: total += 30; // September
+            case 9:  total += 31; // August
+            case 8:  total += 31; // July
+            case 7:  total += 30; // June
+            case 6:  total += 31; // May
+            case 5:  total += 30; // April
+            case 4:  total += 31; // March
+            case 3:  total += 28; // February
+            case 2:  total += 31; // January
+            case 1:  break;
+            default: valid = false;
+        }
+
+        if (valid) System.out.println("Day number = " + total);
+        else System.out.println("Invalid month");
+    }
+}
